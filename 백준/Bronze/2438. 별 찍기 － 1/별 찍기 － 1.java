3a3c8d4811e73f4a1250ff17d25b4b840d99e4e0
강지민 <84773954+jmk307@users.noreply.github.com>
2024-07-27 17:19:04 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 228 ms, Memory: 14892 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
