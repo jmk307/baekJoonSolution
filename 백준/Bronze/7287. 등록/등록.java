5bceec621710483313046a62cfe23a0a7b42c257
강지민 <84773954+jmk307@users.noreply.github.com>
2024-08-20 19:05:28 +0900

    [Bronze V] Title: 등록, Time: 104 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println(186);
+        System.out.println("jimin112688");
+    }
+}
