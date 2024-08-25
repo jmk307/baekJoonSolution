b26df5168eb1a4556185f6ca1aeb777e00e3d669
강지민 <84773954+jmk307@users.noreply.github.com>
2024-08-25 14:44:53 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    static int N, M;
+
+    static boolean[] visited;
+    static int[] matrix;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        matrix = new int[N + 1];
+
+        DFS(0, 0);
+        System.out.println(sb);
+    }
+
+    static void DFS(int depth, int prev) {
+        if (depth == M) {
+            for (int i = 1; i < depth + 1; i++) {
+                sb.append(matrix[i]).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            if (!visited[i] && i > prev) {
+                visited[i] = true;
+                matrix[depth + 1] = i;
+                DFS(depth + 1, i);
+
+                visited[i] = false;
+            }
+        }
+    }
+}
