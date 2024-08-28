2f50bc84a9a170079f6552f06e78388e5d50fe49
강지민 <84773954+jmk307@users.noreply.github.com>
2024-08-28 16:40:06 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 128 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    static int N;
+    
+    static int[] matrix;
+    static int[] dp;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        
+        matrix = new int[N];
+        dp = new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            matrix[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int max = 0;
+        
+        for (int i = 0; i < N; i++) {
+            dp[i] = matrix[i];
+            
+            for (int j = 0; j < i; j++) {
+                if (matrix[j] < matrix[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + matrix[i]);
+                }
+            }
+            
+            max = Math.max(max, dp[i]);
+        }
+        
+        System.out.println(max);
+    }
+}
