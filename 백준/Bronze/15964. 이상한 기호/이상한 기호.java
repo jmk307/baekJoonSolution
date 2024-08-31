189c4f7cca11e7cfda7cdcc7c76f190a0eb7c0d6
강지민 <84773954+jmk307@users.noreply.github.com>
2024-08-31 18:37:30 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14144 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    static int A, B;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        long answer = (A + B) * (A - B);
+
+        System.out.println(answer);
+    }
+}
