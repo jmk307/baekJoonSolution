5cdaa90fcca12dfe0f9fa71bd7f294d6d3e8c552
강지민 <84773954+jmk307@users.noreply.github.com>
2024-10-10 18:41:02 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static int T, A, B;
+    
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        T = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            
+            System.out.println("yes");
+        }
+    }
+}
