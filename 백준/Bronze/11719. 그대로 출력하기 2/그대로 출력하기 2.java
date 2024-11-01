a399409a1d1b0de36039d425ddb367f35c6aa53b
강지민 <84773954+jmk307@users.noreply.github.com>
2024-11-01 18:47:32 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main
+{
+    static int N, M, c, x, h;
+
+    static List<Long> list = new ArrayList<>();
+    static int[][] matrix;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
