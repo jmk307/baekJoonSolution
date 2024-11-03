f66882c0d05d7e30b19845561c3e1bc0ded36705
강지민 <84773954+jmk307@users.noreply.github.com>
2024-11-03 14:00:10 +0900

    [Silver II] Title: 민균이의 계략, Time: 136 ms, Memory: 14712 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main
+{
+    static int N;
+
+    static int[] matrix, dp;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        matrix = new int[N];
+        dp = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            matrix[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (matrix[j] < matrix[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.println(max);
+    }
+}
