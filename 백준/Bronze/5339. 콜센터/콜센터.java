7e4c14413a9dfb75d6c1390458ad5db341285414
강지민 <84773954+jmk307@users.noreply.github.com>
2024-12-13 17:14:13 +0900

    [Bronze V] Title: 콜센터, Time: 92 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static int n, m, a, b;
+    static int count = 0;
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    static ArrayList<Integer>[] matrix;
+    static boolean[] visited;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
