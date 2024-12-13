b1f72cb7e8ae99745276f032b1d992142e9acb36
강지민 <84773954+jmk307@users.noreply.github.com>
2024-12-13 17:14:42 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 96 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static int n, m, a, b;
+    static int count = 0;
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    static ArrayList<Integer>[] matrix;
+    static boolean[] visited;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println("고려대학교");
+    }
+}
