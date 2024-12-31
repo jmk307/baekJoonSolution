18a6993e67b7a8b48f0f32c1598c950926c0be51
강지민 <84773954+jmk307@users.noreply.github.com>
2024-12-31 21:36:09 +0900

    [Bronze V] Title: 새로운 시작, Time: 112 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static int N;
+
+    static int[] dz = {0, 0, 0, 0, 1, -1};
+    static int[] dx = {-1, 0, 1, 0, 0, 0};
+    static int[] dy = {0, -1, 0, 1, 0, 0};
+
+    static ArrayList<Integer>[] matrix;
+    static boolean[] broken;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println("파이팅!!");
+    }
+}
