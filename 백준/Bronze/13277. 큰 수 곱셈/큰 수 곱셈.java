0f1b1906fca303ec49568ddaacb55b306a8f8c99
강지민 <84773954+jmk307@users.noreply.github.com>
2025-01-01 21:20:59 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5540 ms, Memory: 406980 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static int N;
+
+    static int[] dz = {0, 0, 0, 0, 1, -1};
+    static int[] dx = {-1, 0, 1, 0, 0, 0};
+    static int[] dy = {0, -1, 0, 1, 0, 0};
+
+    static ArrayList<Integer>[] matrix;
+    static boolean[] broken;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+        System.out.println(a.multiply(b));
+    }
+}
