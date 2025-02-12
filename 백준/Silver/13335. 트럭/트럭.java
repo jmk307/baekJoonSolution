1ac36091245dacb9457bd83005ebd33a56f803d2
강지민 <84773954+jmk307@users.noreply.github.com>
2025-02-12 17:36:57 +0900

    [Silver I] Title: 트럭, Time: 124 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,70 +1,72 @@
 import java.math.BigInteger;
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 
 public class Main {
-	static int n, w, L;
-	static long min = Integer.MAX_VALUE;
-	
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, -1, 0, 1};
-	
-	static int[] matrix;
-	static Queue<Integer> que = new LinkedList<>();
+    static int n, w, L;
+    static int min = 1000 * 1000;
 
-	static StringBuilder sb = new StringBuilder();
+    static int[] dz = {0, 0, 0, 0, 1, -1};
+    static int[] dx = {0, 1, 0, -1, 1, 1, -1, -1};
+    static int[] dy = {1, 0, -1, 0, 1, -1, 1, -1};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		w = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		
-		matrix = new int[n];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-		    matrix[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for (int i = 1; i <= w; i++) {
-		    que.add(0);
-		}
-		
-		int count = 0;
-		int index = 0;
-		
-		int total = w;
-		int sum = 0;
-		
-		while (true) {
-		    if (index == n) {
-		        break;
-		    }
-		    
-		    count++;
-		    
-		    if (!que.isEmpty()) {
-		        total--;
-		        sum -= que.poll();
-		    }
-		    
-		    if (total < w) {
-		        total++;
-		        
-		        if (sum + matrix[index] <= L) {
-		            sum += matrix[index];
-		            que.add(matrix[index]);
-		            index++;
-		        } else {
-		            que.add(0);
-		        }
-		    }
-		}
-		
-		System.out.println(que.size() + count);
-	}
+    static int[] matrix;
+    static int[][] dist;
+    static boolean[][] visited;
+    static Deque<Integer> deque = new ArrayDeque<>();
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        matrix = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            matrix[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int index = 0;
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            if (index == n) {
+                break;
+            }
+
+            int size = deque.size();
+            if (deque.isEmpty()) {
+                deque.add(matrix[index]);
+                sum += matrix[index];
+                index++;
+            } else {
+                if (size == w) {
+                    sum -= deque.poll();
+                }
+
+                if (sum + matrix[index] <= L) {
+                    deque.add(matrix[index]);
+                    sum += matrix[index];
+                    index++;
+                } else {
+                    deque.add(0);
+                }
+            }
+
+            count++;
+        }
+
+        count += w;
+
+        System.out.println(count);
+    }
 }
