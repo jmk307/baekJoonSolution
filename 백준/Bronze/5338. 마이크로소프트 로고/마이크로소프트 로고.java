8116805d82f3aec0765c5793015b5dac161d92dc
강지민 <84773954+jmk307@users.noreply.github.com>
2025-02-26 16:43:31 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 92 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,31 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    static int n, m;
+
+    static int[] dz = {0, 0, 0, 0, 1, -1};
+    static int[] dx = {0, 1, 0, -1, 1, 1, -1, -1};
+    static int[] dy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+    static int[] matrix, dp;
+    static boolean[][] visited;
+    static int[][] dist;
+    static ArrayList<Integer>[] list;
+    static Set<Integer>[] set;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
